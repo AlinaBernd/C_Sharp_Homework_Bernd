9e7c799dc01fed7ff592f71c9a5ce43f7634f4c6
alina bernd <a-manaeva@mail.ru>
2023-07-22 19:59:34 +0300

    Добавила код программы для задачи 4 в Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 Console.Write ("Inrut a first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
@@ -12,4 +12,23 @@ else
 {
     Console.WriteLine("Max = " + num1);
 }
+
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 */
+Console.Write ("Inrut a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Inrut a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Inrut a third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (max <= num2)
+{
+    max = num2;
+}
+if (max <= num3)
+{
+    max = num3;
+}
+Console.WriteLine(max);
+
