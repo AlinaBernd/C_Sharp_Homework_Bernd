9942ab1a2bc2781e2cdc1056bb2b8903f4bed408
alina bernd <a-manaeva@mail.ru>
2023-07-23 14:53:43 +0300

    Добавила код программы для задачи 8 в Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-
+*/
 Console.Write ("Inrut a first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Inrut a second number: ");
@@ -12,9 +12,9 @@ else
 {
     Console.WriteLine("Max = " + num1);
 }
-
+/*
 Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-
+*/
 Console.Write ("Inrut a first number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Inrut a second number: ");
@@ -31,7 +31,7 @@ if (max <= num3)
     max = num3;
 }
 Console.WriteLine(max);
-
+/*
 Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 */
 Console.Write ("Inrut a number: ");
@@ -44,4 +44,17 @@ else
 {
     Console.WriteLine("No");
 }
-
+/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+*/
+Console.Write("Inrut a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int evenNumber = 2;
+if (number >= 2)
+{
+    while (evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
